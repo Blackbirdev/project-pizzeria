a410d1dc3186607f3c9ad6bd7b6b71693d03754c
Bartłomiej Kos <bart.blackbird@gmail.com>
2023-03-08 12:27:59 +0100

    Fix default option for price calculated

--- dist/js/script.js
+++ dist/js/script.js
@@ -147,13 +147,13 @@
           // check if there is param with a name of paramId in formData and if it includes optionId
           if (formData[paramId] && formData[paramId].includes(optionId)) {
             // check if the option is not default
-            if (optionId.default != true) {
+            if (option.default != true) {
               // add option price to price variable
               price += option.price;
             }
           } else {
             // check if the option is default
-            if (optionId.default == true) {
+            if (option.default == true) {
               // reduce price variable
               price -= option.price;
             }
