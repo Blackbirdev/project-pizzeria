cf256117d649dd9e2402fd5c8e0618c9efe3df86
Bartłomiej Kos <bart.blackbird@gmail.com>
2023-03-11 11:26:42 +0100

    Find widget elements

--- dist/js/script.js
+++ dist/js/script.js
@@ -63,6 +63,7 @@
       thisProduct.initOrderForm();
       thisProduct.initAmountWidget();
       thisProduct.processOrder();
+
       // console.log('new Product:', thisProduct);
     }
     renderInMenu() {
@@ -187,9 +188,18 @@
   class AmountWidget {
     constructor(element) {
       const thisWidget = this;
+      thisWidget.getElements(element);
 
       console.log('AmountWidget:', thisWidget);
       console.log('constructor argument:', element);
+    }
+    getElements(element) {
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
   }
 
